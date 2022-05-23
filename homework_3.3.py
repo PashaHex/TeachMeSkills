e819bbc12a557367e0793382244f8142a60cab42
Pavel Niakhai <3055963@mail.ru>
2022-05-23 12:18:38 +0300

    add read and write issue

--- homework_3.3.py
+++ homework_3.3.py
@@ -1,13 +1,23 @@
 STRATEGY = input('Введите стратегию для оаботы приложения(FIFO / LIFO)')
 
-cont = []
+with open('data.txt', 'r', encoding='utf-8') as f:
+    cont = list(map(lambda x: x.strip(), f.readlines()))
+    
+print(cont)
+
 while True:
     el = input('Что у вас?')
     if el:
-        if STRATEGY.upper() == 'LIFO':
+        if el.upper() == 'LIFO':
+            STRATEGY.upper() == 'LIFO'
+        elif el.upper() == 'FIFO':
+            STRATEGY.upper() == 'FIFO'
+        elif el == 'end':
+            break
+        elif STRATEGY.upper() == 'LIFO':
             cont.append(el)
         elif STRATEGY.upper() == 'FIFO':
-            cont.inser(0, el)
+            cont.insert(0, el)        
         else:
             print('Введите корректный тип сортировки')
         print('Спасибо!')
@@ -15,4 +25,10 @@ while True:
         print('Примите наши извинения, но в данный момент мы ничего не можем вам предложить. Заходите позже.')
     else:
         item = cont.pop()
-        print(f'Возьмите, вот вам {item}')
+        print(f'Возьмите, вот вам - {item}')
+
+cont = list(map(lambda x: x + '\n', cont))
+print('Вышли из цикла', cont)
+
+with open('data.txt', 'w', encoding='utf-8') as file:
+    file.writelines(cont)
