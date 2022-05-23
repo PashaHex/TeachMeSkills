a95ff9656ad19a31c07b5f7b1703251501d5210d
Pavel Niakhai <3055963@mail.ru>
2022-05-23 12:44:03 +0300

    fix same issue

--- homework_3.3.py
+++ homework_3.3.py
@@ -1,4 +1,4 @@
-STRATEGY = input('Введите стратегию для оаботы приложения(FIFO / LIFO)')
+STRATEGY = input('Введите стратегию для оаботы приложения(FIFO / LIFO)').upper()
 
 with open('data.txt', 'r', encoding='utf-8') as f:
     cont = list(map(lambda x: x.strip(), f.readlines()))
@@ -9,17 +9,20 @@ while True:
     el = input('Что у вас?')
     if el:
         if el.upper() == 'LIFO':
-            STRATEGY.upper() == 'LIFO'
+            STRATEGY = 'LIFO'
+            continue
         elif el.upper() == 'FIFO':
-            STRATEGY.upper() == 'FIFO'
+            STRATEGY = 'FIFO'
+            continue
         elif el == 'end':
             break
-        elif STRATEGY.upper() == 'LIFO':
+        elif STRATEGY == 'LIFO':
             cont.append(el)
-        elif STRATEGY.upper() == 'FIFO':
+        elif STRATEGY == 'FIFO':
             cont.insert(0, el)        
-        else:
+        elif STRATEGY != 'FIFO' or SRTATEGY != 'LIFO':
             print('Введите корректный тип сортировки')
+            continue
         print('Спасибо!')
     elif not cont:
         print('Примите наши извинения, но в данный момент мы ничего не можем вам предложить. Заходите позже.')
